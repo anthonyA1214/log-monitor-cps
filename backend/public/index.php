52b3cfb643b0328cb2101ac5cbe183586eed0b50
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-05-17 11:41:09 +0800

    feat: add log management functionality with LogController and LogService

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,18 +5,17 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+$app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
-
 $app->add(new CorsMiddleware($app->getResponseFactory()));
 
-
-require __DIR__ . '/../src/Routes/api.php';
+$routes = require __DIR__ . '/../src/Routes/api.php';
+$routes($app);
 
 $app->run();
