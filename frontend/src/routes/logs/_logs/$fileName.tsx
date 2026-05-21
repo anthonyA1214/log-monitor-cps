bb44472d15915646b8c2fa2b49eac07f78b46325
Nicky <150995121+LesNicknicky@users.noreply.github.com>
2026-05-21 14:28:18 +0800

    Feat: frontend File viewing

--- frontend/src/routes/logs/_logs/$fileName.tsx
+++ frontend/src/routes/logs/_logs/$fileName.tsx
@@ -1,6 +1,7 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout"
 import { logsQueries } from "@/lib/api/logs"
 import { createFileRoute } from "@tanstack/react-router"
+import { Button } from "@/components/ui/button"
 
 export const Route = createFileRoute("/logs/_logs/$fileName")({
   loader: ({ context: { queryClient }, params: { fileName } }) => {
@@ -14,8 +15,26 @@ function LogsInfoPage() {
 
   return (
     <ContentLayout title={fileName}>
-      {/* place your code here */}
-      <p>Info about log file: {fileName}</p>
+      <Button className="mb-5">Back</Button>
+      <div className="relative rounded-2xl h-120 w-full bg-gray-300">
+        <div className="relative rounded-tl-2xl rounded-tr-2xl bg-gray-700 w-full h-15 flex ">
+          <p  className="relative w-[15%] ml-12 pt-5">
+            <span className="absolute m-auto text-white">{fileName}</span>
+          </p>
+          <div className="  pt-5 relative w-[85%] bg-gray-7 00 rounded-tr-2xl h-15 flex flex-row justify-end space-x-5 space-y-4">
+            <p className="mr-4 text-white">
+              Date Modified: {}Time
+            </p>
+            <p className="mr-4 text-white">
+              Status: {}status - {}duration
+            </p>
+            
+          </div>
+            
+
+
+        </div>
+      </div>
     </ContentLayout>
   )
 }
